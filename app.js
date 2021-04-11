84461e13bdf4f9f9fb9251455424fa2587a8569f
phhu <phhu.git@gmail.com>
2021-04-11 18:01:33 +0100

    logos separated

--- app.js
+++ app.js
@@ -1,14 +1,29 @@
 import React, { useState, useEffect } from 'react'
+// const thesaurusdom = require('thesaurus-dom')
+// global.thesaurusdom = thesaurusdom
+// const wordhippo = require('wordhippo')
+// global.wordhippo = wordhippo
+// const thesaurus = require("thesaurus");
+// global.thesaurus = thesaurus
+// var thesaurus = require('powerthesaurus-api')
+// global.thesaurus = thesaurus
+
+// const Synonymator = require("synonymator");
+// const API_KEY = "f3ec27eab362158709e62541a10e328e";
+// const syn = new Synonymator(API_KEY);
+// global.syn = syn
+import NorthFace from './svgLogos/northFace'
+import Mastercard from './svgLogos/mastercard'
+import Nike from './svgLogos/nike'
 
 const App = () => {
-  let dy = 15
+  const dy = 15
   return (
     <div>
-      <svg  height="200" width="200">
-        
-        <text  x="0" y="16">THE</text>
-        <text  x="0" y={16+dy}>NORTH</text>
-        <text  x="0" y={16+2*dy}>FACE</text>
+      <svg height='600' width='600'>
+        <NorthFace transform="translate(100,0)" />
+        <Mastercard transform="translate(0,200)" />
+        <Nike />
       </svg>
     </div>
   )
