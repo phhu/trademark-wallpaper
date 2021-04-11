b3325261ee5d6e4f329eb51c73396b2efc24edbf
phhu <phhu.git@gmail.com>
2021-04-11 02:38:22 +0100

    deployed

--- app.js
+++ app.js
@@ -1,11 +1,17 @@
-import React, { useState, useEffect }  from "react"
+import React, { useState, useEffect } from 'react'
 
 const App = () => {
+  let dy = 15
+  return (
+    <div>
+      <svg  height="200" width="200">
+        
+        <text  x="0" y="16">THE</text>
+        <text  x="0" y={16+dy}>NORTH</text>
+        <text  x="0" y={16+2*dy}>FACE</text>
+      </svg>
+    </div>
+  )
+}
 
-  return <div>Hello</div>
-};    // <Keyboard />         <button onClick={buttonClicked}>switch</button>
-
-
-
-
-export default App;
+export default App
--- index.js
+++ index.js
@@ -1,10 +1,7 @@
-import React from "react"
-import ReactDOM from "react-dom"
+import React from 'react'
+import ReactDOM from 'react-dom'
 import './style.css'
 
+import App from './app'
 
-import App from "./app"
-
-
-
-ReactDOM.render(<App/>, document.getElementById("root"))
+ReactDOM.render(<App />, document.getElementById('root'))
